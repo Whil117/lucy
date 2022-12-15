7a96c7f2ff77025f697e1cfd0036e73e9aea5af5
Ivan García <miltonivan117@gmail.com>
2022-12-14 20:56:23 -0600

    update imports

--- @components/@atoms/index.ts
+++ @components/@atoms/index.ts
@@ -1,6 +1,6 @@
-export { default as AtomButton } from "@components/@atoms/AtomButton";
-export { default as AtomIcon } from "@components/@atoms/AtomIcon";
-export { default as AtomImage } from "@components/@atoms/AtomImage";
-export { default as AtomLoader } from "@components/@atoms/AtomLoader";
-export { default as AtomText } from "@components/@atoms/AtomText";
-export { default as AtomWrapper } from "@components/@atoms/AtomWrapper";
+export { default as AtomButton } from "./AtomButton";
+export { default as AtomIcon } from "./AtomIcon";
+export { default as AtomImage } from "./AtomImage";
+export { default as AtomLoader } from "./AtomLoader";
+export { default as AtomText } from "./AtomText";
+export { default as AtomWrapper } from "./AtomWrapper";
